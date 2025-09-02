ff6a19352d716509da053e854f3bade0395061a0
Ark2 <90465200+averyark@users.noreply.github.com>
2025-09-02 18:44:24 +0800

    last

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -1,16 +1,5 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Note
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}} # Ensure password is write-only
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create", views.NoteListCreate.as_view(), name="note-list-create"),
+    path("", views.NoteListCreate.as_view(), name="note-list-create"),
     path("delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 ]
