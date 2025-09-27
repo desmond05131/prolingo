3d7c9fea9f5b0db68fa1aff33ab7b1e32b300ca3
JungleDome <jungledome1905@gmail.com>
2025-09-28 01:29:18 +0800

    commit

--- backend/courses/serializers/admin/user_course.py
+++ backend/courses/serializers/admin/user_course.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from courses.models import UserCourse
 
 class AdminUserCourseSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source="user.username", read_only=True)
+    course_title = serializers.CharField(source="course.title", read_only=True)
     class Meta:
         model = UserCourse
         fields = [
@@ -10,5 +12,7 @@ class AdminUserCourseSerializer(serializers.ModelSerializer):
             "course",
             "enrollment_date",
             "is_dropped",
+            "username",
+            "course_title",
         ]
         read_only_fields = ["user_course_id", "enrollment_date"]
